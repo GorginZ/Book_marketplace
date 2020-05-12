42d0637a95ffe131afa12048d3942a9140057285
GorginZ <georgia.z.leng@gmail.com>
2020-05-12 17:16:44 +1000

    navbar boostrap working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -11,9 +12,11 @@ require("channels")
 import "bootstrap"
 import "../stylesheets/application"
 
-document.addEventListener("turbolinks:load"), () => {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+  $('.toast').toast({ autohide: false })
+  $('#toast').toast('show')
 })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
