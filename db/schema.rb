0acaad4c4b0a244fc84657572750161ac3bc3a34
GorginZ <georgia.z.leng@gmail.com>
2020-05-14 18:43:11 +1000

    categories forms view working now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_050257) do
+ActiveRecord::Schema.define(version: 2020_05_14_083726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_050257) do
     t.string "book_category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "summary"
   end
 
   create_table "listings", force: :cascade do |t|
