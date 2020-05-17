0f5b54c0e30b3289e16c4332182089550a490586
GorginZ <georgia.z.leng@gmail.com>
2020-05-17 15:50:37 +1000

    search works for title, now adding other parameters, author ISBN category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
     
 def index
 end
+
+def search
+    if params[:search].blank?
+      redirect_to(root_path, alert: 'Empty field!')
+    else
+      @parameter = params[:search].downcase
+      @listings = Listing.all.where('lower(title) LIKE :search', search: "%#{@parameter}%")
+    end
+  end
+
+
+
+
 end
