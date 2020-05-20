e63de435c5e0efa22c769c0d30066ecd0af74d15
GorginZ <georgia.z.leng@gmail.com>
2020-05-20 13:38:28 +1000

    aws changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   has_many :listings
-  
+  has_one_attached :picture
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
     belongs_to :user #dependent destroy
     has_one :category
     has_one :order
+    has_one_attached :picture
 end
