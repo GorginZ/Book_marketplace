02ec2c5eeaf25260bc78b47b1695bd0f03a3ce6f
GorginZ <georgia.z.leng@gmail.com>
2020-05-21 18:34:00 +1000

    listings controller code comments

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,19 +7,23 @@ class ListingsController < ApplicationController
 
   # GET /listings
   # GET /listings.json
+    #The below query is one part of ensuring when a listing is sold it won't render in browsing. 
+    #the other part of this is the webhook action in payments controller. It changes Listing objects attribute 'available' to false and saves the listing object
+  # only listings that are not sold (available = true) will render so there aren't problematic duplicates of liistings
   def index
-    # @listings = Listing.all
-    # only listings that are not sold will render so there aren't problematic duplicates of liistings
     @listings = Listing.where(available: true)
   end
 
   # GET /listings/1
   # GET /listings/1.json
+  #category_id is a forieng key attribute in listings objects which corresponds to the primary key of a category object. This is an interger value of course, so in order to access the english word book_category (eg Law) in the show view (line 20 @category_id.book_category) I need this query to find the correct category object corresponding to the listing. I store this as an instance variable so I can then call .book_category to get the english word title for the instance of the category object
   def show
      @category_id = Category.find_by_id(@listing.category_id)
   end
 
   # GET /listings/new
+  #In my new listing form I have users select a book category from a drop down menu. This is to prevent user input errors and to safegruard against duplications in the database but it also makes things easier for the seller user. 
+  #The instance variable @all_categories stores a collection of all Category objects' book_category (the english word titles and the category_ids and the cateogry_id is what is actually assigned to the Listing object as a foreign key attribute, but the book_category is needed as it is the English Word title that the viewers can select from.
   def new
     @listing = Listing.new
     @all_categories = Category.all.collect{ |c| [c.book_category, c.id] }
@@ -27,6 +31,8 @@ class ListingsController < ApplicationController
   end
 
   # GET /listings/1/edit
+  #same as above re all_categoryes
+  #authorization called. edit action is first argument and the instance of listing object in question is the second argument
   def edit
     @all_categories = Category.all.collect{ |c| [c.book_category, c.id] }
     authorize! :edit, @listing
@@ -35,6 +41,7 @@ class ListingsController < ApplicationController
 
   # POST /listings
   # POST /listings.json
+  #the new instance of listing object gets the following attributes: it belongs to the current_user and it is also fed params from the above new method as arguments (collected in the view form). The new action receives the attributes of the new instance of the listing object as the arguments (listing_params)
   def create
     @listing = current_user.listings.new(listing_params)
 
