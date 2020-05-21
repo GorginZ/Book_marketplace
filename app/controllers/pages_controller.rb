f88e90eddd295d92f26a9688f024c009b6f176f9
GorginZ <georgia.z.leng@gmail.com>
2020-05-21 15:02:35 +1000

    comments in categories controller logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,13 @@ def index
   @listings = Listing.where(available: true)
 
 end
-
+#search sanitizes user input by seperating the variable search string from the conditions string in line with rails docs reccomendations for search safety. Search queries for listings title, author, keywords and ISBN for comprehensive search of available listings.
 def search
     if params[:search].blank?
       redirect_to(root_path, alert: 'Empty field!')
     else
       @parameter = params[:search].downcase
       @listings = Listing.all.where('(title||author||keywords||isbn) LIKE :search', search: "%#{@parameter}%")
-
     end
   end
 
