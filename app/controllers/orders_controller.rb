73b35682ae411f6fc8e2b187a7cf44f4e6542349
GorginZ <georgia.z.leng@gmail.com>
2020-05-24 20:18:17 +1000

    rubo cop and wire frames added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,22 @@
 class OrdersController < ApplicationController
-#return to make admin orders page leave for now
-def index
+  # return to make admin orders page leave for now
+  def index
     # authorize! :read, @orders
     # @orders = Orders.all
-end
-#new instance of order class object.
-def new
+  end
+
+  # new instance of order class object.
+  def new
     @order = Order.new
+  end
 
-end
-#the orders instance variable is the current user's orders.
-def my_orders
+  # the orders instance variable is the current user's orders.
+  def my_orders
     @orders = current_user.orders
-end
-#same as above
-def show
+  end
+
+  # same as above
+  def show
     @orders = current_user.orders
-
+  end
 end
-
-end
-
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,18 @@
 class PagesController < ApplicationController
- #only listings that are not sold will be visible  
- #the with_picture_attached scope ues pre_loading under the hood and greatly reduces database queries.
-def index
-  @listings = Listing.where(available: true).with_attached_picture.all.limit(100)
+  # only listings that are not sold will be visible
+  # the with_picture_attached scope ues pre_loading under the hood and greatly reduces database queries.
+  def index
+    @listings = Listing.where(available: true).with_attached_picture.all.limit(100)
+  end
 
-end
-#search sanitizes user input by seperating the variable's search string from the condition query string in line with rails docs reccomendations for search safety. Search queries for listings title, author, keywords and ISBN for comprehensive search of available listings.
-#utilised preloading for images of search result listings as with all renderings of categories and lisings elsewhere, as the search is broad - it makes a difference if it's able to be made faster.
-def search
+  # search sanitizes user input by seperating the variable's search string from the condition query string in line with rails docs reccomendations for search safety. Search queries for listings title, author, keywords and ISBN for comprehensive search of available listings.
+  # utilised preloading for images of search result listings as with all renderings of categories and lisings elsewhere, as the search is broad - it makes a difference if it's able to be made faster.
+  def search
     if params[:search].blank?
       redirect_to(root_path, alert: 'Empty field!')
     else
       @parameter = params[:search].downcase
       @listings = Listing.with_attached_picture.all.where('(title||author||keywords||isbn) LIKE :search', search: "%#{@parameter}%")
     end
-  end
-
-
+    end
 end
